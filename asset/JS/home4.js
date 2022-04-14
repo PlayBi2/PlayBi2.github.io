d3a1c2d25c31262c0c62457b9fc5a21373e1336f
PlayBi <85682158+PlayBi@users.noreply.github.com>
2022-04-14 18:28:18 +0700

    ewf

--- asset/JS/home4.js
+++
@@ -1,8 +0,0 @@
-var AtributeBTNs = document.getElementsByClassName('selection-item-link');
-
-for(var i = 0 ;i < AtributeBTNs.length-1; i++){
-    AtributeBTNs[i].onclick = function(){
-        AtributeBTNs[i].style.background = "green";
-        AtributeBTNs[i].style.color = "#fff";
-    } 
-}
