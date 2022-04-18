a42a1b32ea57258a15756872ccc767a5caa6b23d
PlayBi <85682158+PlayBi@users.noreply.github.com>
2022-04-18 22:55:41 +0700

    fdsà

--- asset/JS/font-end/index2.js
+++
@@ -1,8 +0,0 @@
-var NameValue = sessionStorage.getItem('NameMember');
-var LoginUserName = document.getElementById('User-name');
-LoginUserName.innerHTML = NameValue;
-
-var manage = document.getElementById('quanly');
-manage.onclick = function(){
-    window.open('http://127.0.0.1:5500/web-item/QuanTri.html');
-}
